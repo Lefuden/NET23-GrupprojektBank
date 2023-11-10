f4cf158cc157284432a53ee215ac0bbd94804681
Tobias Skog <t_ott_e@hotmail.com>
2023-11-10 12:46:44 +0100

    Added the Bcrypt NugetPackage to the project

--- Users/User.cs
+++ Users/User.cs
@@ -2,6 +2,16 @@
 {
     internal class User
     {
+        private Guid UserId { get; set; }
+        private string Salt { get; set; }
+        private string HashedPassword { get; set; }
 
+        public User()
+        {
+            UserId = Guid.NewGuid();
+            Salt = BCrypt.Net.BCrypt.GenerateSalt();
+
+            Console.WriteLine(UserId);
+        }
     }
 }
