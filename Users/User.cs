db6a294e64a8d2f00e4b98d892dce300e313740e
Lefuden <84024382+Lefuden@users.noreply.github.com>
2023-11-10 19:15:07 +0100

    Added properties and unimplemented methods for User, Customer and Admin classes.

--- Users/User.cs
+++ Users/User.cs
@@ -1,17 +1,46 @@
-﻿namespace NET23_GrupprojektBank.Users
-{
-    internal class User
-    {
-        private Guid UserId { get; set; }
-        private string Salt { get; set; }
-        private string HashedPassword { get; set; }
+﻿using NET23_GrupprojektBank.Users.UserInformation;
+using NET23_GrupprojektBank.Managers.Logs;
 
-        public User()
+namespace NET23_GrupprojektBank.Users
+{
+    internal abstract class User
+    {
+        protected string UserName { get; set; }
+        protected Guid UserId { get; set; }
+        protected string Salt { get; set; }
+        protected string HashedPassword { get; set; }
+        protected PersonInformation Person { get; set; }
+        protected Enum UserType { get; set; }
+        protected List<Log> Logs { get; set; }
+        protected enum Type
         {
+            Customer,
+            Admin,
+            Undeclared
+        }
+        
+        protected User(string userName, string password)
+        {
+            UserName = userName;
+            Person = new PersonInformation();
             UserId = Guid.NewGuid();
             Salt = BCrypt.Net.BCrypt.GenerateSalt();
-
-            Console.WriteLine(UserId);
+            HashedPassword = BCrypt.Net.BCrypt.HashPassword(password + Salt);
+            Logs = new List<Log>();
+            UserType = Type.Undeclared;
         }
+
+        //work in progress - direction/help might be wanted
+        //internal void Addlog(string userName, Log log)
+        //{
+        //    if (!Logs.Contains(userName))
+        //    {
+        //        //create new log?
+        //    }
+        //    else
+        //    {
+        //        Logs.Add(userName);
+        //    }
+        //}
     }
 }
