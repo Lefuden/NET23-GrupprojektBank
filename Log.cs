ea0cc21093c020bbc0b2f6a2ce214cc5adfd7734
Lefuden <84024382+Lefuden@users.noreply.github.com>
2023-11-10 09:48:50 +0100

    Add project files.

---
+++ Log.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET23_GrupprojektBank
+{
+    internal class Log
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NET23_GrupprojektBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
