7d5a9fe3e937b87e4240b6e9c568c6a038c8a647
Tobias Skog <t_ott_e@hotmail.com>
2023-11-11 14:57:09 +0100

    Added the eventstatus enum to the project

---
+++ Managers/EventStatus.cs
@@ -0,0 +1,36 @@
+﻿namespace NET23_GrupprojektBank.Managers
+{
+    public enum EventStatus
+    {
+        LoginSuccess,
+        LoginFailed,
+        LoginLocked,
+        CurrencyExchangeRateUpdateSuccess,
+        CurrencyExchangeRateUpdateFailed,
+        CheckingCreationSuccess,
+        CheckingCreationFailed,
+        SavingsCreationSuccess,
+        SavingCreationFailed,
+        TransactionSuccess,
+        TransactionFailed,
+        DepositSuccess,
+        DepositFailed,
+        WithdrawalSuccess,
+        WithdrawalFailed,
+        TransferSuccess,
+        TransferFailed,
+        LoanSuccess,
+        LoanFailed,
+        AccountCreationSuccess,
+        AccountCreationFailed,
+        AdressSuccess,
+        AdressFailed,
+        EmailSuccess,
+        EmailFailed,
+        PhoneSuccess,
+        PhoneFailed,
+        ContactInformationSuccess,
+        ContactInformationFailed,
+        InvalidInput
+    }
+}
