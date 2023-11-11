708694243a325085d20bad3b0e5df312ea74e151
Tobias Skog <t_ott_e@hotmail.com>
2023-11-11 14:46:40 +0100

    Started working on LoginManager

--- Managers/Login/LoginManager.cs
+++ Managers/Login/LoginManager.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NET23_GrupprojektBank.Users;
 
 namespace NET23_GrupprojektBank.Managers.Login
 {
+    public enum LoginStatus
+    {
+        Success,
+        Failed,
+        Locked
+    }
     internal class LoginManager
     {
+        private List<User> Users { get; set; }
+        private int LoginAttempts { get; set; } = 3;
+        private bool IsLocked { get; set; } = false;
+
+        public LoginManager()
+        {
+            Users = new();
+        }
+
+        public LoginStatus Login(string userName, string password)
+        {
+            Users.ForEach(user =>
+            {
+                if (user.ExistingUser(userName))
+                {
+                    return true;
+                }
+            });
+
+
+            return LoginStatus.Failed;
+        }
     }
 }
