c69be6cf2e4916288340f7836128c777f57b6d6b
Tobias Skog <t_ott_e@hotmail.com>
2023-11-12 02:54:57 +0100

    Started working with exchangerate-api to get and update the exchange rates for 3 dictionaries, SEK, USD and EUR and the main currency. Added the Newtonsoft Json nuget package to handle Dezerializing of objects.

--- Currency/CurrencyExchangeRate.cs
+++ Currency/CurrencyExchangeRate.cs
@@ -1,21 +1,203 @@
-﻿using NET23_GrupprojektBank.Users;
+﻿using NET23_GrupprojektBank.Currency.DTO;
+using NET23_GrupprojektBank.Users;
+using Newtonsoft.Json;
+using System.Reflection;
 
 namespace NET23_GrupprojektBank.Currency
 {
     internal static class CurrencyExchangeRate
     {
-        private static Dictionary<CurrencyType, decimal> CurrentExchangeRates { get; set; }
-        public static void UpdateCurrencyExchange(UserType userType)
+        private static Dictionary<CurrencyType, double> CurrentExchangeRatesSEK { get; set; } = new();
+        private static Dictionary<CurrencyType, double> CurrentExchangeRatesUSD { get; set; } = new();
+        private static Dictionary<CurrencyType, double> CurrentExchangeRatesEUR { get; set; } = new();
+        private const int AmountOfSupportedCurrencyTypes = 3;
+        private const string SupportedCurrencyTypes = "SEK,USD,EUR";
+        private const string SupportedCurrencyFilePaths = "SEKResponse.txt,USDResponse.txt,EURResponse.txt";
+
+        private static string SEKResponse { get; set; }
+        private static string USDResponse { get; set; }
+        private static string EURResponse { get; set; }
+        public static DTOForCurrencyExchangeRateAPI SEKCurrencyRate { get; set; } = new();
+        public static DTOForCurrencyExchangeRateAPI USDCurrencyRate { get; set; } = new();
+        public static DTOForCurrencyExchangeRateAPI EURCurrencyRate { get; set; } = new();
+        public static async Task UpdateCurrencyExchangeRate(UserType userType)
         {
             if (userType is not UserType.Admin)
             {
                 return;
             }
+            SEKCurrencyRate = SEKCurrencyRate is null ? new DTOForCurrencyExchangeRateAPI() : SEKCurrencyRate;
+            USDCurrencyRate = USDCurrencyRate is null ? new DTOForCurrencyExchangeRateAPI() : USDCurrencyRate;
+            EURCurrencyRate = EURCurrencyRate is null ? new DTOForCurrencyExchangeRateAPI() : EURCurrencyRate;
 
-            // Update the Currency Exchange Rate here
+            Console.Write("Enter your api key: ");
+            string apiKey = Console.ReadLine();
+            string baseApiUrl = "https://v6.exchangerate-api.com/v6";
+
+            // Option for the admin to choose what the main currency exchange rate is based on our CurrencyType Enum > 
+            // Converting Enum Value ToString().ToUpper() since the API request requires all caps
+            string[] currencyTypes = SupportedCurrencyTypes.Split(',');
+            string[] filePaths = SupportedCurrencyFilePaths.Split(',');
+            for (int i = 0; i < AmountOfSupportedCurrencyTypes; i++)
+            {
+
+                using (HttpClient client = new HttpClient())
+                {
+                    try
+                    {
+                        HttpResponseMessage response = await client.GetAsync($"{baseApiUrl}/{apiKey}/latest/{currencyTypes[i]}");
+                        response.EnsureSuccessStatusCode();
+
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        File.WriteAllText(filePaths[i], responseBody);
+                    }
+                    catch (HttpRequestException ex)
+                    {
+                        Console.WriteLine($"Error accessing the API: {ex.Message}");
+                    }
+                }
+            }
+
+            UpdateDictionaries();
+        }
+        public static void UpdateFromFile()
+        {
+            Console.WriteLine($"{SEKCurrencyRate is null} - {USDCurrencyRate is null} - {EURCurrencyRate is null}");
+            SEKCurrencyRate ??= new DTOForCurrencyExchangeRateAPI();
+            USDCurrencyRate ??= new DTOForCurrencyExchangeRateAPI();
+            EURCurrencyRate ??= new DTOForCurrencyExchangeRateAPI();
+
+            UpdateDictionaries();
+        }
+        private static void UpdateDictionaries()
+        {
+            bool SEKUpdatedFromFile = false, USDUpdatedFromFile = false, EURUpdatedFromFile = false;
+            string[] filePaths = SupportedCurrencyFilePaths.Split(',');
+            string SEKPath = filePaths[0];
+            string USDPath = filePaths[1];
+            string EURPath = filePaths[2];
+
+            if (File.Exists(SEKPath))
+            {
+                Console.WriteLine(SEKPath + " Existing File, creating object");
+                SEKResponse = File.ReadAllText(SEKPath);
+                SEKCurrencyRate = JsonConvert.DeserializeObject<DTOForCurrencyExchangeRateAPI>(SEKResponse);
+                SEKUpdatedFromFile = true;
+
+                Type type = SEKCurrencyRate.conversion_rates.GetType();
+                PropertyInfo[] properties = type.GetProperties();
+                Console.WriteLine("SEK Dictionary: ");
+                foreach (var property in properties)
+                {
+                    if (property.PropertyType == typeof(double))
+                    {
+                        // Get the property name and value
+                        string propertyName = property.Name;
+                        double propertyValue = (double)property.GetValue(SEKCurrencyRate.conversion_rates);
+                        CurrencyType currencyType = Enum.Parse<CurrencyType>(propertyName, false);
+                        CurrentExchangeRatesSEK[currencyType] = propertyValue;
+                        // Print the property name and value
+                        Console.WriteLine($"\t{propertyName}: {propertyValue}");
+                    }
+                }
+            }
+            if (File.Exists(USDPath))
+            {
+                Console.WriteLine(USDPath + " Existing File, creating object");
+                USDResponse = File.ReadAllText(USDPath);
+                USDCurrencyRate = JsonConvert.DeserializeObject<DTOForCurrencyExchangeRateAPI>(USDResponse);
+                USDUpdatedFromFile = true;
+
+                Type type = USDCurrencyRate.conversion_rates.GetType();
+                PropertyInfo[] properties = type.GetProperties();
+                Console.WriteLine("USD Dictionary: ");
+                foreach (var property in properties)
+                {
+                    if (property.PropertyType == typeof(double))
+                    {
+                        // Get the property name and value
+                        string propertyName = property.Name;
+                        double propertyValue = (double)property.GetValue(USDCurrencyRate.conversion_rates);
+                        CurrencyType currencyType = Enum.Parse<CurrencyType>(propertyName, false);
+                        CurrentExchangeRatesUSD[currencyType] = propertyValue;
+                        // Print the property name and value
+                        Console.WriteLine($"\t{propertyName}: {propertyValue}");
+                    }
+                }
+            }
+            if (File.Exists(EURPath))
+            {
+                Console.WriteLine(EURPath + " Existing File, creating object");
+                EURResponse = File.ReadAllText(EURPath);
+                EURCurrencyRate = JsonConvert.DeserializeObject<DTOForCurrencyExchangeRateAPI>(EURResponse);
+                EURUpdatedFromFile = true;
+
+                Type type = EURCurrencyRate.conversion_rates.GetType();
+                PropertyInfo[] properties = type.GetProperties();
+                Console.WriteLine("EUR Dictionary: ");
+                foreach (var property in properties)
+                {
+                    if (property.PropertyType == typeof(double))
+                    {
+                        // Get the property name and value
+                        string propertyName = property.Name;
+                        double propertyValue = (double)property.GetValue(EURCurrencyRate.conversion_rates);
+                        CurrencyType currencyType = Enum.Parse<CurrencyType>(propertyName, false);
+                        CurrentExchangeRatesEUR[currencyType] = propertyValue;
+                        // Print the property name and value
+                        Console.WriteLine($"\t{propertyName}: {propertyValue}");
+                    }
+                }
+            }
+
+
+            if (SEKUpdatedFromFile is not true)
+            {
+                InitializeBaseValuesToADictionary(CurrentExchangeRatesSEK);
+            }
+            if (USDUpdatedFromFile is not true)
+            {
+                InitializeBaseValuesToADictionary(CurrentExchangeRatesUSD);
+            }
+            if (EURUpdatedFromFile is not true)
+            {
+                InitializeBaseValuesToADictionary(CurrentExchangeRatesEUR);
+            }
+        }
+
+
+        private static void InitializeBaseValuesToADictionary(Dictionary<CurrencyType, double> exchangeRateDictionary)
+        {
+            foreach (CurrencyType currencyType in Enum.GetValues(typeof(CurrencyType)))
+            {
+                if (exchangeRateDictionary.TryGetValue(currencyType, out double exchangeValue) is not true)
+                {
+                    exchangeRateDictionary.TryAdd(currencyType, 1);
+                }
+                else
+                {
+                    if (exchangeValue != 1)
+                    {
+                        exchangeRateDictionary[currencyType] = 1;
+                    }
+                }
+            }
 
         }
 
-        public static Dictionary<CurrencyType, decimal> GetCurrentCurrencyExchangeRate() => CurrentExchangeRates;
+        public static Dictionary<CurrencyType, double> GetCurrentCurrencyExchangeRate(CurrencyType currencyType)
+        {
+            switch (currencyType)
+            {
+                case CurrencyType.SEK:
+                    return CurrentExchangeRatesSEK;
+                case CurrencyType.USD:
+                    return CurrentExchangeRatesUSD;
+                case CurrencyType.EUR:
+                    return CurrentExchangeRatesEUR;
+                default:
+                    return CurrentExchangeRatesSEK;
+            }
+        }
     }
 }
---
+++ Currency/DTO/DTOForCurrencyExchangeRateAPI.cs
@@ -0,0 +1,15 @@
+﻿namespace NET23_GrupprojektBank.Currency.DTO
+{
+    internal class DTOForCurrencyExchangeRateAPI
+    {
+        public string result { get; set; }
+        public string documentation { get; set; }
+        public string terms_of_use { get; set; }
+        public string time_last_update_unix { get; set; }
+        public string time_last_update_utc { get; set; }
+        public string time_next_update_unix { get; set; }
+        public string time_next_update_utc { get; set; }
+        public string base_code { get; set; }
+        public ConversionRate conversion_rates { get; set; }
+    }
+}
