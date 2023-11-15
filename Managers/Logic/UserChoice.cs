6398c013728f45621c03132ef2b8648c0b5aeca2
Tobias Skog <t_ott_e@hotmail.com>
2023-11-15 02:28:07 +0100

    Big updates to LogicManager, added UserChoice Enum, edited LoginManager and Log a tiny bit

---
+++ Managers/Logic/UserChoice.cs
@@ -0,0 +1,28 @@
+﻿namespace NET23_GrupprojektBank.Managers.Logic
+{
+    public enum UserChoice
+    {
+        Login,
+        Back,
+        Exit,
+        CreateTransaction,
+        MakeWithdrawal,
+        MakeTransfer,
+        MakeLoan,
+        GetAccounts,
+        ViewWelcomeMenu,
+        ViewCustomerMenu,
+        ViewAdminMenu,
+        ViewLogs,
+        ViewLockedMenu,
+        ViewBalance,
+        CreateBankAccount,
+        CreateChecking,
+        CreateSavings,
+        CreateUser,
+        CreateCustomer,
+        CreateAdmin,
+        UpdateCurrencyExchange,
+        Invalid,
+    }
+}
--- Managers/Logs/Log.cs
+++ Managers/Logs/Log.cs
@@ -5,7 +5,7 @@ namespace NET23_GrupprojektBank.Managers.Logs
     internal class Log
     {
         public DateTime DateAndTime { get; set; }
-        public User User { get; set; }
+        public User? User { get; set; }
         public string Message { get; set; }
 
         public Log(DateTime dateAndTime, User user, string message)
@@ -18,6 +18,18 @@ namespace NET23_GrupprojektBank.Managers.Logs
         {
             DateAndTime = DateTime.UtcNow;
             User = user;
+            Message = message;
+        }
+        public Log(DateTime dateAndTime, string message)
+        {
+            DateAndTime = dateAndTime;
+            User = default;
+            Message = message;
+        }
+        public Log(string message)
+        {
+            DateAndTime = DateTime.UtcNow;
+            User = default;
             Message = message;
         }
     }
