1d9cb6b2079ea7b24d53c66634fb0c2d53d788bb
Lefuden <84024382+Lefuden@users.noreply.github.com>
2023-11-15 11:40:06 +0100

    started working on user account creation in admin.cs

--- Users/User.cs
+++ Users/User.cs
@@ -32,21 +32,10 @@ namespace NET23_GrupprojektBank.Users
             UserType = UserType.Undeclared;
         }
 
-        internal bool CompareUserName(string userName)
-        {
-            return userName == UserName;
-        }
-
-        internal bool CompareUserPassword(string userPassword)
-        {
-            return BCrypt.Net.BCrypt.Verify(userPassword + Salt, HashedPassword);
-        }
-
-        internal void Addlog(EventStatus eventStatus)
-        {
-            Logs.Add(new Log(DateTime.Now, this, GetLogMessage(eventStatus)));
-
-        }
+        internal bool CompareUserName(string userName) => userName == UserName;
+        internal bool CompareUserPassword(string userPassword) => BCrypt.Net.BCrypt.Verify(userPassword + Salt, HashedPassword);
+        internal void Addlog(EventStatus eventStatus) => Logs.Add(new Log(DateTime.Now, this, GetLogMessage(eventStatus)));
+        
         public void ShowLog(List<Log> list)
         {
             foreach (var o in list)
@@ -65,31 +54,31 @@ namespace NET23_GrupprojektBank.Users
                     EventStatus.AdressFailed => $"{UserName} failed to add address",
                     EventStatus.AdressSuccess => $"{UserName} has added an address",
                     EventStatus.CheckingCreationFailed => $"{UserName} checking account creation failed",
-                    EventStatus.CheckingCreationSuccess => $"{UserName} checking account creation is a great success",
-                    EventStatus.ContactInformationFailed => $"{UserName} ContactInformationFailed",
-                    EventStatus.ContactInformationSuccess => $"{UserName} ContactInformationSuccess",
-                    EventStatus.CurrencyExchangeRateUpdateFailed => $"{UserName} CurrencyExchangeRateUpdateFailed",
-                    EventStatus.CurrencyExchangeRateUpdateSuccess => $"{UserName} CurrencyExchangeRateUpdateSuccess",
-                    EventStatus.DepositFailed => $"{UserName} DepositFailed",
-                    EventStatus.DepositSuccess => $"{UserName} DepositSuccess",
-                    EventStatus.EmailFailed => $"{UserName} EmailFailed",
-                    EventStatus.EmailSuccess => $"{UserName} EmailSuccess",
-                    EventStatus.InvalidInput => $"{UserName} InvalidInput",
-                    EventStatus.LoanFailed => $"{UserName} LoanFailed",
-                    EventStatus.LoanSuccess => $"{UserName} LoanSuccess",
-                    EventStatus.LoginFailed => $"{UserName} LoginFailed",
-                    EventStatus.LoginSuccess => $"{UserName} LoginSuccess",
-                    EventStatus.LoginLocked => $"{UserName} LoginLocked",
-                    EventStatus.PhoneFailed => $"{UserName}PhoneFailed",
-                    EventStatus.PhoneSuccess => $"{UserName}PhoneSuccess",
-                    EventStatus.SavingCreationFailed => $"{UserName}SavingCreationFailed",
-                    EventStatus.SavingsCreationSuccess => $"{UserName} SavingsCreationSuccess",
-                    EventStatus.TransactionFailed => $"{UserName} TransactionFailed",
-                    EventStatus.TransactionSuccess => $"{UserName} TransactionSuccess",
-                    EventStatus.TransferFailed => $"{UserName} TransferFailed",
-                    EventStatus.TransferSuccess => $"{UserName} TransferSuccess",
-                    EventStatus.WithdrawalFailed => $"{UserName} WithdrawalFailed",
-                    EventStatus.WithdrawalSuccess => $"{UserName} WithdrawalSuccess",
+                    EventStatus.CheckingCreationSuccess => $"{UserName} successfully created a checking account",
+                    EventStatus.ContactInformationFailed => $"{UserName} failed to add contact information",
+                    EventStatus.ContactInformationSuccess => $"{UserName} successfully added contact information",
+                    EventStatus.CurrencyExchangeRateUpdateFailed => $"{UserName} currency exchange rate update failed",
+                    EventStatus.CurrencyExchangeRateUpdateSuccess => $"{UserName} currency exchange rate update success",
+                    EventStatus.DepositFailed => $"{UserName} deposit failed",
+                    EventStatus.DepositSuccess => $"{UserName} deposit success",
+                    EventStatus.EmailFailed => $"{UserName} failed to add email",
+                    EventStatus.EmailSuccess => $"{UserName} email has been added",
+                    EventStatus.InvalidInput => $"{UserName} invalid input",
+                    EventStatus.LoanFailed => $"{UserName} bank loan failed",
+                    EventStatus.LoanSuccess => $"{UserName} bank loan success",
+                    EventStatus.LoginFailed => $"{UserName} login failed",
+                    EventStatus.LoginSuccess => $"{UserName} successfully logged in",
+                    EventStatus.LoginLocked => $"{UserName} login locked",
+                    EventStatus.PhoneFailed => $"{UserName} failed to add phone number",
+                    EventStatus.PhoneSuccess => $"{UserName} phone number has been added",
+                    EventStatus.SavingCreationFailed => $"{UserName} savings account creation failed",
+                    EventStatus.SavingsCreationSuccess => $"{UserName} savings account creation success",
+                    EventStatus.TransactionFailed => $"{UserName} transaction failed",
+                    EventStatus.TransactionSuccess => $"{UserName} transaction success",
+                    EventStatus.TransferFailed => $"{UserName} transfer failed",
+                    EventStatus.TransferSuccess => $"{UserName} transfer success",
+                    EventStatus.WithdrawalFailed => $"{UserName} withdrawal failed, outside of balance bounds",
+                    EventStatus.WithdrawalSuccess => $"{UserName} withdrawal approved",
                     _ => $"{UserName} something has gone terribly wrong"
                 };
             }
