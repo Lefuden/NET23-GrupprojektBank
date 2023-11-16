3d5dfc8f0eb653dda3047caac45b1bb3df748d69
Tobias Skog <t_ott_e@hotmail.com>
2023-11-16 09:55:20 +0100

    Added a step by step guide for transactiomanager

--- Managers/Transactions/TransactionsManager.cs
+++ Managers/Transactions/TransactionsManager.cs
@@ -32,8 +32,24 @@
         {
             while (Transactions.Count > 0)
             {
-                var transaction = Transactions.Dequeue();
-                // Handle the transaction here
+                Transaction transaction = Transactions.Dequeue();
+
+                // Vilken transaction type är det?
+
+                // Beroende på transaction type så hantera det korrekt
+
+                // är source och destination currency type densamma?
+
+                // Är dem samma så behöver du inte göra en konvertering
+
+                // är dem OLIKA MÅSTE du göra en konvertering av sum till Destination Currency Type
+
+                // Se till att pengar går från rätt konto till rätt konto
+
+                // Se till att dra av pengar från kontot om det behövs
+
+                // Kontrollera att det finns rätt mängd pengar på kontot innan du flyttar pengar
+
             }
         }
     }
