3185ebc3b2e4290085bd930db495170a0d03d48d
Lefuden <84024382+Lefuden@users.noreply.github.com>
2023-11-21 18:02:24 +0100

    Vastly improved Account creation

--- Users/Admin.cs
+++ Users/Admin.cs
@@ -17,150 +17,218 @@ namespace NET23_GrupprojektBank.Users
 
         public Admin CreateAdminAccount(List<string> existingUsernames)
         {
+            Email email = new("","");
+            Phone phone = new("");
+            Address adress = new("","","","");
+            List<string> choices = new()
+            {
+                "Email",
+                "Phone",
+                "Adress",
+                "Complete",
+                "Back"
+            };
+
             var (username, password, firstName, lastName, dateOfBirth) = UserCommunications.GetBasicsFromUser(existingUsernames);
-            if (username == "-1") return null;
-
-            if (!UserCommunications.AskUserYesOrNo("Add email information?"))
+            if (username == "-1")
             {
-                AnsiConsole.MarkupLine("[green]Admin has been created[/]");
-                AddLog(EventStatus.AccountCreationSuccess);
-                UserCommunications.FakeBackChoice("Ok");
-                return new Admin(username, password,
-                       new PersonInformation(firstName, lastName, dateOfBirth,
-                       new ContactInformation(new Email(""))));
+                StopUserCreation("Admin creation cancelled", EventStatus.AccountCreationFailed);
+                return null;
             }
 
-            Email email = UserCommunications.GetEmailFromUser();
-            if (email.EmailAddress == "-1")
+            while (true)
             {
-                Console.Clear();
-                if (!UserCommunications.AskUserYesOrNo("Continue account creation without email?"))
+                string stringChoice = AnsiConsole.Prompt(new SelectionPrompt<string>()
+                    .Title($"[purple]Select field[/]")
+                    .PageSize(5)
+                    .AddChoices(choices));
+                
+                switch (stringChoice)
                 {
-                    return null;
+                    case "Email":
+                        email = UserCommunications.GetEmailFromUser();
+                        if (email.EmailAddress == "-1")
+                        {
+                            choices.Remove("Email");
+                            Console.Clear();
+                            if (!UserCommunications.AskUserYesOrNo("Continue account creation without email?"))
+                            {
+                                StopUserCreation("Admin creation cancelled", EventStatus.AccountCreationFailed);
+                                return null;
+                            }
+                        }
+                        else if (email != null)
+                        {
+                            choices.Remove("Email");
+                        }
+                        break;
+
+                    case "Phone":
+                        phone = UserCommunications.GetPhoneFromUser();
+                        if (phone.PhoneNumber == "-1")
+                        {
+                            Console.Clear();
+                            if (!UserCommunications.AskUserYesOrNo("Continue account creation without phone number?"))
+                            {
+                                StopUserCreation("Admin creation cancelled", EventStatus.AccountCreationFailed);
+                                return null;
+                            }
+                        }
+                        else if (phone != null)
+                        {
+                            choices.Remove("Phone");
+                        }
+                        break;
+
+                    case "Adress":
+                        adress = UserCommunications.GetAdressFromUser();
+                        if (adress.Country == "-1")
+                        {
+                            choices.Remove("Adress");
+                            Console.Clear();
+                            if (!UserCommunications.AskUserYesOrNo("Continue account creation without adress?"))
+                            {
+                                StopUserCreation("Admin creation cancelled", EventStatus.AccountCreationFailed);
+                                return null;
+                            }
+                        }
+                        else if (adress != null)
+                        {
+                            choices.Remove("Adress");
+                        }
+                        break;
+
+                    case "Complete":
+                        StopUserCreation("New admin has been created", EventStatus.AccountCreationSuccess);
+                        return new Admin(username, password,
+                               new PersonInformation(firstName, lastName, dateOfBirth,
+                               new ContactInformation(email, phone, adress)));
+                    
+                    case "Back":
+                        StopUserCreation("Admin creation cancelled", EventStatus.AccountCreationFailed);
+                        return null;
+                }
+
+                if (choices.Count == 2)
+                {
+                    StopUserCreation("New admin has been created", EventStatus.AccountCreationSuccess);
+                    return new Admin(username, password,
+                           new PersonInformation(firstName, lastName, dateOfBirth,
+                           new ContactInformation(email, phone, adress)));
                 }
             }
-
-            if (!UserCommunications.AskUserYesOrNo("Add phone information?"))
-            {
-                AnsiConsole.MarkupLine("[green]Admin has been created[/]");
-                AddLog(EventStatus.AccountCreationSuccess);
-                UserCommunications.FakeBackChoice("Ok");
-                return new Admin(username, password,
-                       new PersonInformation(firstName, lastName, dateOfBirth,
-                       new ContactInformation(email)));
-            }
-
-            Phone phone = UserCommunications.GetPhoneFromUser();
-            if (phone.PhoneNumber == "-1")
-            {
-                Console.Clear();
-                if (!UserCommunications.AskUserYesOrNo("Continue account creation without phone number?"))
-                {
-                    return null;
-                }
-            }
-
-            if (!UserCommunications.AskUserYesOrNo("Add adress information?"))
-            {
-                AnsiConsole.MarkupLine("[green]Admin has been created[/]");
-                AddLog(EventStatus.AccountCreationSuccess);
-                UserCommunications.FakeBackChoice("Ok");
-                return new Admin(username, password,
-                       new PersonInformation(firstName, lastName, dateOfBirth,
-                       new ContactInformation(email, phone)));
-            }
-
-            Address adress = UserCommunications.GetAdressFromUser();
-            if (adress.Country == "-1")
-            {
-                Console.Clear();
-                if (!UserCommunications.AskUserYesOrNo("Continue account creation without adress?"))
-                {
-                    return null;
-                }
-            }
-
-            AnsiConsole.MarkupLine("[green]Admin has been created[/]");
-            AddLog(EventStatus.AccountCreationSuccess);
-            UserCommunications.FakeBackChoice("Ok");
-            return new Admin(username, password,
-                   new PersonInformation(firstName, lastName, dateOfBirth,
-                   new ContactInformation(email, phone, adress)));
         }
 
         public Customer CreateCustomerAccount(List<string> existingUsernames)
         {
+            Email email = new("","");
+            Phone phone = new("");
+            Address adress = new("","","","");
+            List<string> choices = new()
+            {
+                "Email",
+                "Phone",
+                "Adress",
+                "Complete",
+                "Back"
+            };
+
             var (username, password, firstName, lastName, dateOfBirth) = UserCommunications.GetBasicsFromUser(existingUsernames);
-            if (username == "-1") return null;
-
-            if (!UserCommunications.AskUserYesOrNo("Add email information?"))
+            if (username == "-1")
             {
-                AnsiConsole.MarkupLine("[green]Customer has been created[/]");
-                AddLog(EventStatus.AccountCreationSuccess);
-                UserCommunications.FakeBackChoice("Ok");
-                return new Customer(username, password,
-                       new PersonInformation(firstName, lastName, dateOfBirth,
-                       new ContactInformation(new Email(""))));
+                StopUserCreation("Customer creation cancelled", EventStatus.AccountCreationFailed);
+                return null;
             }
 
-            Email email = UserCommunications.GetEmailFromUser();
-            if (email.EmailAddress == "-1")
+            while (true)
             {
-                Console.Clear();
-                if (!UserCommunications.AskUserYesOrNo("Continue account creation without email?"))
+                string stringChoice = AnsiConsole.Prompt(new SelectionPrompt<string>()
+                    .Title($"[purple]Select field[/]")
+                    .PageSize(5)
+                    .AddChoices(choices));
+                
+                switch (stringChoice)
                 {
-                    return null;
+                    case "Email":
+                        email = UserCommunications.GetEmailFromUser();
+                        if (email.EmailAddress == "-1")
+                        {
+                            choices.Remove("Email");
+                            Console.Clear();
+                            if (!UserCommunications.AskUserYesOrNo("Continue account creation without email?"))
+                            {
+                                StopUserCreation("Customer creation cancelled", EventStatus.AccountCreationFailed);
+                                return null;
+                            }
+                        }
+                        else if (email != null)
+                        {
+                            choices.Remove("Email");
+                        }
+                        break;
+
+                    case "Phone":
+                        phone = UserCommunications.GetPhoneFromUser();
+                        if (phone.PhoneNumber == "-1")
+                        {
+                            Console.Clear();
+                            if (!UserCommunications.AskUserYesOrNo("Continue account creation without phone number?"))
+                            {
+                                StopUserCreation("Customer creation cancelled", EventStatus.AccountCreationFailed);
+                                return null;
+                            }
+                        }
+                        else if (phone != null)
+                        {
+                            choices.Remove("Phone");
+                        }
+                        break;
+
+                    case "Adress":
+                        adress = UserCommunications.GetAdressFromUser();
+                        if (adress.Country == "-1")
+                        {
+                            choices.Remove("Adress");
+                            Console.Clear();
+                            if (!UserCommunications.AskUserYesOrNo("Continue account creation without adress?"))
+                            {
+                                StopUserCreation("Customer creation cancelled", EventStatus.AccountCreationFailed);
+                                return null;
+                            }
+                        }
+                        else if (adress != null)
+                        {
+                            choices.Remove("Adress");
+                        }
+                        break;
+
+                    case "Complete":
+                        StopUserCreation("New customer has been created", EventStatus.AccountCreationSuccess);
+                        return new Customer(username, password,
+                               new PersonInformation(firstName, lastName, dateOfBirth,
+                               new ContactInformation(email, phone, adress)));
+                    
+                    case "Back":
+                        StopUserCreation("Customer creation cancelled", EventStatus.AccountCreationFailed);
+                        return null;
+                }
+
+                if (choices.Count == 2)
+                {
+                    StopUserCreation("New customer has been created", EventStatus.AccountCreationSuccess);
+                    return new Customer(username, password,
+                           new PersonInformation(firstName, lastName, dateOfBirth,
+                           new ContactInformation(email, phone, adress)));
                 }
             }
-
-            if (!UserCommunications.AskUserYesOrNo("Add phone information?"))
-            {
-                AnsiConsole.MarkupLine("[green]Customer has been created[/]");
-                AddLog(EventStatus.AccountCreationSuccess);
-                UserCommunications.FakeBackChoice("Ok");
-                return new Customer(username, password,
-                       new PersonInformation(firstName, lastName, dateOfBirth,
-                       new ContactInformation(email)));
-            }
-
-            Phone phone = UserCommunications.GetPhoneFromUser();
-            if (phone.PhoneNumber == "-1")
-            {
-                Console.Clear();
-                if (!UserCommunications.AskUserYesOrNo("Continue account creation without phone number?"))
-                {
-                    return null;
-                }
-            }
-
-            if (!UserCommunications.AskUserYesOrNo("Add adress information?"))
-            {
-                AnsiConsole.MarkupLine("[green]Customer has been created[/]");
-                UserCommunications.FakeBackChoice("Ok");
-                AddLog(EventStatus.AccountCreationSuccess);
-                return new Customer(username, password,
-                       new PersonInformation(firstName, lastName, dateOfBirth,
-                       new ContactInformation(email, phone)));
-            }
-
-            Address adress = UserCommunications.GetAdressFromUser();
-            if (adress.Country == "-1")
-            {
-                Console.Clear();
-                if (!UserCommunications.AskUserYesOrNo("Continue account creation without adress?"))
-                {
-                    return null;
-                }
-            }
-
-            AnsiConsole.MarkupLine("[green]Customer has been created[/]");
-            AddLog(EventStatus.AccountCreationSuccess);
-            UserCommunications.FakeBackChoice("Ok");
-            return new Customer(username, password,
-                   new PersonInformation(firstName, lastName, dateOfBirth, 
-                   new ContactInformation(email, phone, adress)));
         }
 
+        public void StopUserCreation(string message, EventStatus status)
+        {
+            AnsiConsole.MarkupLine($"[green]{message}[/]");
+            AddLog(status);
+            UserCommunications.FakeBackChoice("Ok");
+        }
         public void UpdateCurrencyExchangeRate()
         {
             EventStatus taskUpdateStatus = CurrencyExchangeRate.UpdateCurrencyExchangeRateAsync(UserType).Result;
