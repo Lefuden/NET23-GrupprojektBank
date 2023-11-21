8418dece7394f000a636175bb8fd3b2c7aafc574
Lefuden <84024382+Lefuden@users.noreply.github.com>
2023-11-21 13:36:20 +0100

    UserCommunication.cs: MakeDepositMenu() probably done

--- Managers/UserInteraction/UserCommunicationsDaniel.cs
+++ Managers/UserInteraction/UserCommunicationsDaniel.cs
@@ -12,26 +12,16 @@ namespace NET23_GrupprojektBank.Managers.UserInteraction
             WriteDivider($"Deposit Menu");
             var accountChoices = GetBankAccountInfo(bankAccounts);
             accountChoices.AccountInformationList.Add("Back");
-            var selectedAccountChoice = AnsiConsole.Prompt(
-                new SelectionPrompt<string>()
+            var selectedAccountChoice = AnsiConsole.Prompt(new SelectionPrompt<string>()
                     .PageSize(15)
                     .Title($"Select an account to deposit to {accountChoices.SelectionPromtTitel}")
-                    .AddChoices(accountChoices.AccountInformationList)
-            );
+                    .AddChoices(accountChoices.AccountInformationList));
 
             string pattern = @"\[red bold\](\d+)\[/\]";
             Regex regex = new Regex(pattern);
 
-            int choosenAccountNumber = GetSingleMatch(pattern, selectedAccountChoice);
-            var selectedAccount = bankAccounts.FirstOrDefault(account =>
-            {
-                if (account.GetAccountNumber() == choosenAccountNumber)
-                {
-                    return true;
-                }
-
-                return false;
-            });
+            int chosenAccountNumber = GetSingleMatch(pattern, selectedAccountChoice);
+            var selectedAccount = bankAccounts.FirstOrDefault(account => account.GetAccountNumber() == chosenAccountNumber);
 
             if (selectedAccount == null)
             {
@@ -40,51 +30,25 @@ namespace NET23_GrupprojektBank.Managers.UserInteraction
             }
 
             var info = selectedAccount.GetAccountInformation();
-            var accountName = info.Name;
-            var balance = info.Balance;
-            var currencyType = info.Currency;
-
-            AnsiConsole.MarkupLine($"[purple]Account name:[/] [green]{accountName}[/]");
-            AnsiConsole.MarkupLine($"[purple]Balance:[/] [blue]{balance}[/] [gold1]{currencyType}[/]");
-            AnsiConsole.MarkupLine($"[purple]Deposit to:[/]  [green]{accountName}[/]");
-
+            WriteTransactionInformation(info);
+            
             while (true)
             {
-                string input = AnsiConsole.Ask<string>($"[purple]How much would you like to deposit?[/]");
+                decimal depositAmount = AnsiConsole.Ask<decimal>($"[purple]How much would you like to deposit?[/]");
 
-                if (!decimal.TryParse(input, out decimal depositAmount) || depositAmount <= 0)
+                if (depositAmount >= 0)
                 {
-                    AnsiConsole.MarkupLine($"[red]Please enter a valid deposit amount[/]");
-                }
-                else
-                {
-                    decimal currentBalance = decimal.Parse(balance);
+                    decimal currentBalance = decimal.Parse(info.Balance);
                     currentBalance += depositAmount;
-                    balance = currentBalance.ToString();
+                    info.Balance = currentBalance.ToString();
 
-                    AnsiConsole.MarkupLine(
-                        $"[green]{depositAmount:c} {currencyType}[/] [purple]has been deposited to {accountName}[/]");
-
-                    string stringChoice = AnsiConsole.Prompt(
-                        new SelectionPrompt<string>()
-                            .PageSize(3)
-                            .AddChoices(new[]
-                                {
-                                    "Ok"
-                                }
-                            ));
-
-                    if (stringChoice == "Ok")
-                    {
-                        CurrencyType currencyTypeParsed = (CurrencyType)Enum.Parse(typeof(CurrencyType),
-                            currencyType ?? CurrencyType.SEK.ToString());
-                        return (selectedAccount, currencyTypeParsed, DateTime.UtcNow, depositAmount);
-                    }
-                    else
-                    {
-                        return default;
-                    }
+                    AnsiConsole.MarkupLine($"[green]{depositAmount:c} {info.Currency}[/] [purple]has been deposited to {info.Name}[/]\n");
+                    FakeBackChoice("Ok");
+                    
+                    CurrencyType currencyTypeParsed = (CurrencyType)Enum.Parse(typeof(CurrencyType), info.Currency ?? CurrencyType.SEK.ToString());
+                    return (selectedAccount, currencyTypeParsed, DateTime.UtcNow, depositAmount);
                 }
+                AnsiConsole.MarkupLine($"[red]Please enter a valid deposit amount[/]");
             }
         }
     }
